94d767009d2b9def458db29267e64f6caed6652c
Jhudeska <jvsignacia@live.nl>
2019-05-01 17:29:21 +0200

    juiste versie van de index.php pagina

---
+++ car_details.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jhudeska
+ * Date: 2019-05-01
+ * Time: 17:27
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title> Jhudeska | Auto Webshop</title>
+</head>
+<body>
+
+<h1 class="h1_center">Opdracht voor Jhudeska</h1>
+<p class="p_description">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
+
+<div class="container">
+
+
+    <div class="model-preview-block">
+
+        <div class="model-preview-index-large first">
+            <div class="title"><h2>FABIA</h2></div>
+            <div class="type">Active</div>
+            <div class="description"> Stoere basis</div>
+            <div class="price">Vanaf €15.830</div>
+            <button class="btn btn-primary">Bekijk onze Fabia's</button>
+            <div class="image"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+        </div>
+    </div>
+
+
+    <div class="model-preview-block">
+
+        <div class="model-preview-index-small middle">
+            <div class="title"><h2>KODIAQ</h2></div>
+            <div class="type">Comfort</div>
+            <button class="btn btn-primary">Bekijk onze Kodiaq's</button>
+            <div class="image-middle"><img src="img/Skoda_Kodiaq.jpg" alt=""></div>
+        </div>
+    </div>
+
+
+    <div class="model-preview-block">
+
+        <div class="model-preview-index-large last">
+            <div class="title"><h2>OCTAVIA</h2></div>
+            <div class="type">Comfort</div>
+            <div class="description">Profiteer nu</div>
+            <div class="price">Vanaf €24.890</div>
+            <button class="btn btn-primary">Bekijk onze Octavia's</button>
+            <div class="image"><img src="img/Skodia_Octavia.jpg" alt=""></div>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
