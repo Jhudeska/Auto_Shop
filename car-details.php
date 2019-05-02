f18e7018c3de7e54fbb7196536e0a92d4ca79a41
Jhudeska <jvsignacia@live.nl>
2019-05-03 01:24:01 +0200

    setup layout page 2 car-details

---
+++ car-details.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jhudeska
+ * Date: 2019-05-01
+ * Time: 17:27
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/car-details.css">
+<!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">-->
+    <title> Jhudeska | Auto Webshop</title>
+</head>
+<body>
+
+<h1 class="h1-center">Opdracht voor Jhudeska</h1>
+<p class="p-description">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
+
+<div class="container">
+    <div class="model-preview-block">
+        <div class="model-preview-index-large-first">
+            <div class="title">FABIA HATCHBACK</div>
+            <div class="type">Active</div>
+        </div>
+        <div class="demo-2-first"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+        <div class="text-first">
+            <div class="description-1">Standaard op de Active:</div>
+            <div class="description"> Laadruimte tot 1.150 liter</div>
+            <div class="description"> LED-daverlichting</div>
+            <div class="description"> 6 Airbags</div>
+            <div class="price-1"> Stoere basis</div>
+            <div class="price">Vanaf €15.830</div>
+            <button class="btn btn-green">Bestel deze variant</button>
+        </div>
+
+    </div>
+
+    <div class="model-preview-block">
+        <div class="model-preview-index-large-middle">
+            <div class="title">FABIA HATCHBACK</div>
+            <div class="type">Active</div>
+        </div>
+        <div class="demo-2-middle"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+        <div class="text-middle">
+            <div class="description-1">Standaard op de Active:</div>
+            <div class="description"> Laadruimte tot 1.150 liter</div>
+            <div class="description"> LED-daverlichting</div>
+            <div class="description"> 6 Airbags</div>
+            <div class="price-1"> Stoere basis</div>
+            <div class="price">Vanaf €15.830</div>
+            <button class="btn btn-green">Bestel deze variant</button>
+        </div>
+
+    </div>
+
+
+    <div class="model-preview-block">
+        <div class="model-preview-index-large-last">
+            <div class="title">FABIA HATCHBACK</div>
+            <div class="type">Active</div>
+        </div>
+        <div class="demo-2-last"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+        <div class="text-last">
+            <div class="description-1">Standaard op de Active:</div>
+            <div class="description"> Laadruimte tot 1.150 liter</div>
+            <div class="description"> LED-daverlichting</div>
+            <div class="description"> 6 Airbags</div>
+            <div class="price-1"> Stoere basis</div>
+            <div class="price">Vanaf €15.830</div>
+            <button class="btn btn-green">Bestel deze variant</button>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="gray-background">
+    <h1 id="gray" class="h1-gray">Ik ontvang graag een offerte Skoda Fabia Hatchback </h1>
+    <h1 class="h1-gray">Business Edition </h1>
+
+    <h1 id="gray" class="h2-gray"> €1445 </h1>
+    <h3 id="gray" class="h2-gray">Is dit uw lease bedrag per maand? Wij willen u graag een verblijvende  offerte aanbieden </h3>
+
+    <h1 id="gray" class="h2-gray">VUL JE GEGEVENS IN </h1>
+    <label class="radio">One
+        <input type="radio" checked="checked" name="radio">
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+    <br><br>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <br><br>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <br><br>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <br><br>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <br><br>
+
+    <label class="radio">One
+        <input type="radio" checked="checked" name="radio">
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+    <br><br>
+    <button class="btn btn-green">Bestel deze variant</button>
+</div>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -18,23 +18,21 @@
 </head>
 <body>
 
-<h1 class="h1_center">Opdracht voor Jhudeska</h1>
-<p class="p_description">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
+<h1 class="h1-center">Opdracht voor Jhudeska</h1>
+<p class="p-description">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
 
 <div class="container">
 
 
     <div class="model-preview-block">
-
-        <div class="model-preview-index-large first">
-            <div class="polygon">.</div>
-            <div class="title"><h2>FABIA</h2></div>
+        <div class="model-preview-index-large-first">
+            <div class="title"><h2 class="h2-position">FABIA</h2></div>
             <div class="type">Active</div>
             <div class="description"> Stoere basis</div>
             <div class="price">Vanaf €15.830</div>
-            <button class="btn btn-primary">Bekijk onze Fabia's</button>
-            <div class="image"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+            <button class="btn btn-white">Bekijk onze Fabia's</button>
         </div>
+        <div class="demo-2-first"><img src="img/Skoda_Fabia.jpg" alt=""></div>
     </div>
 
 
@@ -43,22 +41,20 @@
         <div class="model-preview-index-small middle">
             <div class="title"><h2>KODIAQ</h2></div>
             <div class="type">Comfort</div>
-            <button class="btn btn-primary">Bekijk onze Kodiaq's</button>
-            <div class="image-middle"><img src="img/Skoda_Kodiaq.jpg" alt=""></div>
+            <button class="btn btn-white">Bekijk onze Kodiaq's</button>
         </div>
+        <div class="demo-2-middle"><img src="img/Skoda_Kodiaq.jpg" alt=""></div>
     </div>
 
-
     <div class="model-preview-block">
-
-        <div class="model-preview-index-large last">
+        <div class="model-preview-index-large-last">
             <div class="title"><h2>OCTAVIA</h2></div>
             <div class="type">Comfort</div>
             <div class="description">Profiteer nu</div>
             <div class="price">Vanaf €24.890</div>
-            <button class="btn btn-primary">Bekijk onze Octavia's</button>
-            <div class="image"><img src="img/Skodia_Octavia.jpg" alt=""></div>
+            <button class="btn btn-white">Bekijk onze Octavia's</button>
         </div>
+        <div class="demo-2-last"><img src="img/Skodia_Octavia.jpg" alt=""></div>
     </div>
 
 
