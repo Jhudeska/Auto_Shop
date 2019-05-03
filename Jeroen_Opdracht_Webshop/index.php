b5825b6e739d2553a3149d998898e28e51187a89
Jhudeska <jvsignacia@live.nl>
2019-05-03 16:16:31 +0200

    update dev.Jeroen_Opdracht_Webshop

--- Jeroen_Opdracht_Webshop/index.php
+++ Jeroen_Opdracht_Webshop/index.php
@@ -14,7 +14,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/main.css">
-    <title> Jhudeska | Auto Webshop</title>
+    <title> Jeroen_Opdracht_Webshop</title>
 </head>
 <body>
 
