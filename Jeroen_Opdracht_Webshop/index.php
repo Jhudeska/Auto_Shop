4cfebc046911f3b7f5169cd3a03e53f099605d46
Jhudeska <jvsignacia@live.nl>
2019-05-03 15:54:11 +0200

    devide dev.Jeroen_Opdracht_Webshop and Jeroen_Opdracht_Webshop

---
+++ Jeroen_Opdracht_Webshop/index.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jhudeska
+ * Date: 29/04/2019
+ * Time: 21:00
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title> Jhudeska | Auto Webshop</title>
+</head>
+<body>
+
+<h1 class="h1-center">Opdracht voor Jhudeska</h1>
+<p class="p-description">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
+
+<div class="container">
+
+
+    <div class="model-preview-block">
+        <div class="model-preview-index-large-first">
+            <div class="title"><h2 class="h2-position">FABIA</h2></div>
+            <div class="type">Active</div>
+            <div class="description"> Stoere basis</div>
+            <div class="price">Vanaf €15.830</div>
+            <button class="btn btn-white">Bekijk onze Fabia's</button>
+        </div>
+        <div class="demo-2-first"><img src="img/Skoda_Fabia.jpg" alt=""></div>
+    </div>
+
+
+    <div class="model-preview-block">
+
+        <div class="model-preview-index-small middle">
+            <div class="title"><h2>KODIAQ</h2></div>
+            <div class="type">Comfort</div>
+            <button class="btn btn-white">Bekijk onze Kodiaq's</button>
+        </div>
+        <div class="demo-2-middle"><img src="img/Skoda_Kodiaq.jpg" alt=""></div>
+    </div>
+
+    <div class="model-preview-block">
+        <div class="model-preview-index-large-last">
+            <div class="title"><h2>OCTAVIA</h2></div>
+            <div class="type">Comfort</div>
+            <div class="description">Profiteer nu</div>
+            <div class="price">Vanaf €24.890</div>
+            <button class="btn btn-white">Bekijk onze Octavia's</button>
+        </div>
+        <div class="demo-2-last"><img src="img/Skodia_Octavia.jpg" alt=""></div>
+    </div>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
