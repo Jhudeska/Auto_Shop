ea00b1fcb1d9caff647a571ca6487cf7de4c5c02
Jhudeska <jvsignacia@live.nl>
2019-05-10 16:47:34 +0200

    Added Bootstrap project in General folder

--- dev.Jeroen_Opdracht_Web/general/demo_envoirment/index_demo.php
+++ dev.Jeroen_Opdracht_Web/general/demo_envoirment/index_demo.php
@@ -6,29 +6,213 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="css/demo.css">
+<!--    <link rel="stylesheet" href="css/demo.css">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-
-
-<div class="container">
-    <header class="header">
-        <div class="header__text-box">
-            <h1 class="heading-primary">
-                <span class="heading-primary--main">Outdoors</span>
-                <span class="heading-primary--sub">is where life happens</span>
-            </h1>
-
-            <button class="btn btn-white">Bekijk onze Fabia's</button>
-        </div>
-    </header>
-    <img class="demo-2" src="Skoda_Fabia.jpg" width="100" height="140">
-</div>
+    <style>
 
 
 
-<!--<div class="demo-1"> hello</div>-->
-<!--<div class="demo-2">.</div>-->
 
+
+    </style>
+    <div class="container">
+
+        <!-- navbar navbar-default -->
+        <div class="navbar navbar-default" role="navigation">
+
+            <!-- navbar-header -->
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <span class="navbar-brand">Responsive Demo</span>
+
+            </div><!-- /navbar-header -->
+
+            <!-- navbar -->
+            <div class="navbar-collapse collapse" style="height: 1px;">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div><!-- /navbar -->
+
+        </div><!-- /navbar navbar-default -->
+
+        <!-- container --><!-- row -->
+        <div class="row hidden-sm">
+
+            <!-- col-xs-6 -->
+            <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- /col-xs-6 -->
+
+            <!-- col-xs-6 -->
+            <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- /col-xs-6 -->
+
+        </div><!-- /row -->
+
+        <!-- row -->
+        <div class="row">
+
+            <!-- col-sm-6 -->
+            <div class="col-lg-6">
+
+                <p class="visible-xs"><strong>This text should only be viewed by mobile devices.</strong> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-lg-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div><!-- /col-sm-6 -->
+
+        </div><!-- /row -->
+
+        <!-- row -->
+        <div class="row">
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+            <!-- col-sm-6 -->
+            <div class="col-sm-6 col-md-4 col-lg-3">
+
+                <!-- panel panel-default -->
+                <div class="panel panel-default">
+
+                    <!-- panel-body -->
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div><!-- /panel-body -->
+
+                </div><!-- /panel panel-default -->
+
+            </div><!-- /col-sm-6 -->
+
+        </div><!-- /row -->
+
+    </div>
+
+
+    <!-- JQuery (necessary for Bootrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- dev.Jeroen_Opdracht_Web/public_html/header.php
+++ dev.Jeroen_Opdracht_Web/public_html/header.php
@@ -7,11 +7,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">-->
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
     <link rel="shortcut icon" type="image/png" href="../img/kiss-dashboard.png">
      <title> Jhudeska | dev.Jeroen_Opracht</title>
 </head>
 <body>
-<div id="wrapper">
+<!--<div class="container webshop-container">-->
+<!--    <div class="row ">-->
+<!--        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">-->
+<!--            <div class="logo-box">-->
+<!--                <img src="../img/logo@2x.png" alt="logo" class="logo">-->
+<!--            </div>-->
+<!--            <div class="heading-primary">-->
+<!--                <span class="heading-primary-main">Opdracht voor Jhudeska</span>-->
+<!--                <p class="heading-primary-sub">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>-->
+<!--            </div>-->
+<!--        </div>-->
+
+
+<div id="container">
 <header class="header">
     <div class="logo-box">
         <img src="../img/logo@2x.png" alt="logo" class="logo">
@@ -22,4 +38,6 @@
         <p class="heading-primary-sub">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
     </div>
 </header>
+
+
 
