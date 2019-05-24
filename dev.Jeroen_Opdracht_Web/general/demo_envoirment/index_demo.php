9a66f23f8013ddec7469415ce83cfa4af1a137b4
Jhudeska <jvsignacia@live.nl>
2019-05-24 14:21:45 +0200

    added notification of radio btn in git file

--- dev.Jeroen_Opdracht_Web/general/demo_envoirment/index_demo.php
+++ dev.Jeroen_Opdracht_Web/general/demo_envoirment/index_demo.php
@@ -5,211 +5,101 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="refresh" content="3">
     <title>Document</title>
 <!--    <link rel="stylesheet" href="css/demo.css">-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
     <style>
-
-
-
+    .main-form{
+        width: 500px;
+        margin: auto;
+    }
 
 
     </style>
+
     <div class="container">
+            <div class="row">
+                <div class="col">
 
-        <!-- navbar navbar-default -->
-        <div class="navbar navbar-default" role="navigation">
+                    <form action="#" class="main-form needs-validation novalidate">
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="firstname1">Firstname</label>
+                                    <input type="text" name="firstname" id="firstname1" class="form-control" required>
+                                    <small class="form-text text-muted">This should be your first name.</small>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="lastname1">Lastname</label>
+                                        <input type="lastname" name="password" id="lastname1" class="form-control">
+                                </div>
+                            </div>
+                        </div>
 
-            <!-- navbar-header -->
-            <div class="navbar-header">
 
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
 
-                <span class="navbar-brand">Responsive Demo</span>
 
-            </div><!-- /navbar-header -->
+                        <div class="form-group">
+                            <label for="username1">Username</label>
+                            <input type="text" name="username" id="username1" class="form-control is-invalid">
+                            <div>Please enter  a valid username.</div>
+                        </div>
 
-            <!-- navbar -->
-            <div class="navbar-collapse collapse" style="height: 1px;">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
-                </ul>
-            </div><!-- /navbar -->
+                        <div class="form-group">
+                            <label for="password1">Password</label>
+                            <input type="password" name="password" id="password1" class="form-control is-valid">
+                            <div class="invalid-feedback">Looks good!</div>
+                        </div>
 
-        </div><!-- /navbar navbar-default -->
+                        <div class="form-group">
+                            <label for="gender">Gender</label>
+                            <select name="gender" id="gender" class="form-control">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
 
-        <!-- container --><!-- row -->
-        <div class="row hidden-sm">
+                        <div class="form-check">
+                            <input type="checkbox" id="accept-terms" class="form-check-input">
+                            <label for="accept-terms form-check-label">Accept Terms &amp; Conditions</label>
+                        </div>
 
-            <!-- col-xs-6 -->
-            <div class="col-sm-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div><!-- /col-xs-6 -->
+                        <div class="form-check form-check-inline">
+                            <input type="radio" class="form-check-input" id="dhr">
+                            <label for="dhr" class="form-check-label">Dhr.</label>
+                        </div>
 
-            <!-- col-xs-6 -->
-            <div class="col-sm-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div><!-- /col-xs-6 -->
+                        <div class="form-check form-check-inline">
+                            <input type="radio" class="form-check-input" id="mvr">
+                            <label for="mvr" class="form-check-label">Mvr.</label>
+                        </div>
 
-        </div><!-- /row -->
+                        <button class="btn btn-dark text-danger">test</button>
+                    </form>
+                    
 
-        <!-- row -->
-        <div class="row">
-
-            <!-- col-sm-6 -->
-            <div class="col-lg-6">
-
-                <p class="visible-xs"><strong>This text should only be viewed by mobile devices.</strong> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-lg-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div><!-- /col-sm-6 -->
-
-        </div><!-- /row -->
-
-        <!-- row -->
-        <div class="row">
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-            <!-- col-sm-6 -->
-            <div class="col-sm-6 col-md-4 col-lg-3">
-
-                <!-- panel panel-default -->
-                <div class="panel panel-default">
-
-                    <!-- panel-body -->
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </div><!-- /panel-body -->
-
-                </div><!-- /panel panel-default -->
-
-            </div><!-- /col-sm-6 -->
-
-        </div><!-- /row -->
-
+                </div>
+            </div>
     </div>
 
+    <script>
+        var form = document.querySelector('.needs-validator');
+        form.addEventListener('submit', function(event){
+            if(form.checkValidity( == false)){
+                event.preventDefault();
+                event.stopPropagation();
+                form.classList.add('was-validated');
+            }
+        } )
+    </script>
 
+
+    <!-- source https://www.youtube.com/watch?v=K-ccGZYRWzs&feature=youtu.be -->
     <!-- JQuery (necessary for Bootrap's JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
